0cb384b0e7e2a68d7a896ea6cbfd42e0cd8c2672
savannaelbey <savanna_e@icloud.com>
2021-02-28 14:53:00 +0000

    use useeffect and usecontext and render loading page if no product in product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,10 +1,24 @@
-import React, {useContext, useEffect} from 'react';
-// import {ShopContext} from '../context/shopContext';
+import React, { useContext, useEffect } from 'react';
 // import { Container, Text, Div, Row, Col } from 'atomize';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/shopContext';
 
 const ProductPage = () => {
+  //array destructuring
   let { id } = useParams();
+  const { fetchProductWithId, addItemToCheckout, product } = useContext(ShopContext);
+
+  useEffect(() => {
+    fetchProductWithId(id);
+    return () => {
+
+    };
+  }, [ fetchProductWithId, id ])
+
+  if(!product) {
+    return <div>loading...</div>
+  }
+
   return (
     <div>
     PRODUCT PAGE
