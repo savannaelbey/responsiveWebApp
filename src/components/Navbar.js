ae7618a7305765c7964d7dfcb2d4aa2cbce24c5f
savannaelbey <savanna_e@icloud.com>
2021-03-01 14:52:28 +0000

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/shopContext';
 const Navbar = () => {
   const { openCart } = useContext(ShopContext);
   return (
-    <Container>
+    <Container d="flex" flexDir="row" p="2rem" justify="space-between">
       <Link to='/'>Shop</Link>
       <Anchor onClick={() => openCart()}>Shopping Bag</Anchor>
     </Container>
