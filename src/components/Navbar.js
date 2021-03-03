6a8dbd30c8b7f4f377ba509830f57e06eca038f3
savannaelbey <savanna_e@icloud.com>
2021-03-03 18:05:08 +0000

    change navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,31 @@
 import React, { useContext } from 'react';
-import { Container, Icon, Row, Col, Text } from 'atomize';
+import { Container, Icon, Row, Col, Text, Div } from 'atomize';
 import {Link} from 'react-router-dom';
 import { ShopContext } from '../context/shopContext';
 
 const Navbar = () => {
   const { openCart } = useContext(ShopContext);
   return (
-    // <Container d="flex" flexDir="row" p="2rem" justify="space-between">
-    //   <Link to="/"><Icon name="Store" size="30px" color="black" /></Link>
-    //   <Icon onClick={() => openCart()} name="Bag" size="30px" color="black" cursor="pointer" />
-    //   <Icon name="Search" size="30px" cursor="pointer" />
-    // </Container>
-    //
 
+  <Container  p="2rem" >
+  <Row >
 
-  <Container>
-      <Row align="flex-start">
-          <Col>
-            <Icon name="Menu" size="30px" cursor="pointer" />
-          </Col>
-          <Col>
-            <Text> Women </Text>
-          </Col>
-          <Col size={1}>
-            <Link to="/"><Icon name="Store" size="30px" color="black" /></Link>
-          </Col>
-          <Col size={1}>
-            <Icon name="Search" size="30px" cursor="pointer" />
-          </Col>
-          <Col size={1}>
-            <Icon onClick={() => openCart()} name="Bag" size="30px" color="black" cursor="pointer"/>
-          </Col>
-      </Row>
+        <Col size={{xs: 0, sm: 0, md: 0, lg: 1}}>
+          <Text> Women </Text>
+        </Col>
+        <Col size={{xs: 0, sm: 0, md: 0, lg: 1}}>
+          <Text> Men </Text>
+        </Col>
+        <Col bg='black'>
+          <Link to="/"><Icon name="Store" size="30px" color="black" /></Link>
+        </Col>
+        <Col size={{xs: 0, sm: 0, md: 0, lg: 1}}>
+          <Icon name="Search" size="30px" cursor="pointer" />
+        </Col>
+        <Col  size={0}>
+          <Icon   onClick={() => openCart()} name="Bag" size="30px" color="black" cursor="pointer"/>
+        </Col>
+    </Row>
     </Container>
   )
 }
