805d2b1c4c25ba11d9542f01588e4fd2ca855cc0
savannaelbey <savanna_e@icloud.com>
2021-03-04 16:49:35 +0000

    Create returns page and returns process component

---
+++ src/pages/ReturnsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Div, Text } from 'atomize';
+import FAQs from '../components/ReturnsFAQList';
+import ReturnsProcess from '../components/ReturnsProcess';
+
+const ReturnsPage = () => {
+  return (
+    <ReturnsProcess/>
+    <FAQs/>
+  );
+}
+
+export default ReturnsPage;
