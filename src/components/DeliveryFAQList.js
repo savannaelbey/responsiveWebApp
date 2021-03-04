9b05096327f2b2dc060508107fbfe7c766d00cb7
savannaelbey <savanna_e@icloud.com>
2021-03-04 15:48:21 +0000

    Create delivery FAQ list component

---
+++ src/components/DeliveryFAQList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Div } from "atomize";
+import FAQItem from "./DeliveryFAQItem";
+
+const DeliveryFAQList = () => {
+  return (
+    <Div>
+      <FAQItem/>
+      <FAQItem/>
+      <FAQItem/>
+      <FAQItem/>
+    </Div>
+  );
+}
+
+export default DeliveryFAQItem;
--- src/components/ReturnsFAQList.js
+++ src/components/ReturnsFAQList.js
@@ -13,4 +13,4 @@ const ReturnsFAQList = () => {
   );
 }
 
-export default ReturnsFAQList
+export default ReturnsFAQList;
