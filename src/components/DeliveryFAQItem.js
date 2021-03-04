f882b04e48a2cdf8e1176c02d540e445391e3cdf
savannaelbey <savanna_e@icloud.com>
2021-03-04 15:46:04 +0000

    Create Delivery FAQ item component

---
+++ src/components/DeliveryFAQItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Div, Text } from "atomize";
+
+const DeliveryFAQItem = () => {
+  return (
+    <Div>
+      <Text textSize="display1" textAlign="center">Generate Lorem Ipsum placeholder text?</Text>
+      <Text textSize="paragraph" textAlign="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Condimentum id
+      venenatis a condimentum vitae sapien.</Text>
+    </Div>
+  )
+}
+
+export default DeliveryFAQItem;
