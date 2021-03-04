c1e17ad4fb03b09cd1364d1c8d804e9dc87ef509
savannaelbey <savanna_e@icloud.com>
2021-03-04 16:12:28 +0000

    Create womens banner component

---
+++ src/components/WomensBanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Div, Text } from "atomize";
+
+const WomensBanner = () => {
+  return (
+    <Div bg="black900" p="1rem">
+      <Text textSize="title" textAlign="center" p="0.7rem" textColor="white" >Womens</Text>
+    </Div>
+  );
+}
+
+export default WomensBanner;
